e190409b930b05724e29813aee1df94d4585c4e7
Max Estrella <maxestrella565@gmail.com>
2024-07-10 11:30:35 +0100

    Update to player-exercise

--- player-exercise.php
+++ player-exercise.php
@@ -5,12 +5,14 @@
  *
  * Here is a Player class that a colleague started work on. Unfortunately they went on holiday before
  * they could finish it. There are a few issues that are stopping the code from working, and a few extra bits
- * we need you to finish.
+ * we need you to finish. Make sure you test the player as you go by instantiating one and calling it's methods!
+ *
+ * After completing a task please send your code to Ash for feedback :)
  *
  * Tasks
  *
  * __construct()
- * - We need to add a constructor, so we can instantiate Player objects
+ * - We need to add a constructor so we can instantiate Player objects
  * - The only param to the constructor should be maxHealth as an int
  * - The constructor should update the maxHealth property
  * - It should also set the currentHealth to be the same as maxHealth
@@ -52,55 +54,134 @@
  *      - It should increase the currentShield by the shieldAmount
  *          - Unless the currentShield reaches maxShield, in which case any extra is ignored
  *      - It should return the updated currentShield
+ *      - If currentHealth is 0, it should simply return the currentShield and do nothing else
  *
  */
-
 declare(strict_types=1);
 
 
 class Player {
     public int $maxHealth;
     public int $currentHealth;
+    public int $maxShield;
+    public int $currentShield;
     public int $inventoryCapacity = 3;
     public array $inventory = [];
 
-    public function __construct(int $maxHealth, int $currentHealth,)
+    public function __construct(int $maxHealth, int $maxShield)
     {
         $this->maxHealth = $maxHealth;
-        $this->currentHealth = $currentHealth;
+        $this->currentHealth = $maxHealth;
+
+        $this->maxShield = $maxShield;
+        $this->currentShield = $maxShield;
     }
 
     public function takeDamage(int $damageAmount): int
     {
-        return $this->currentHealth -= $damageAmount;
+        // No need to do anything if the player is already dead
+        if ($this->playerIsDead()) {
+            return $this->currentHealth;
+        }
+
+        // We know that they have shield left
+        $playerHasShieldRemaining = $this->currentShield > 0;
+
+        if ($playerHasShieldRemaining) {
+            // Check to see if the damage is less than the shield
+            $shieldIsIntact = $damageAmount <= $this->currentShield;
+
+            if ($shieldIsIntact) {
+                $this->currentShield -= $damageAmount; // Reduce the shield by the damage amount
+                return $this->currentHealth;
+            }
+
+            $damageAmount -= $this->currentShield; // Work out how much damage is left to deal after depleting the shield
+            $this->currentShield = 0; // Deplete the shield
+        }
+
+        $damageExceedsHealth = $this->currentHealth - $damageAmount <= 0;
+
+        if ($damageExceedsHealth) {
+            $this->currentHealth = 0;
+            $this->dead();
+            return $this->currentHealth;
+        }
+
+        $this->currentHealth -= $damageAmount;
+
+        return $this->currentHealth;
+    }
+
+    public function powerupShield(int $shieldAmount): int
+    {
+        if ($this->playerIsDead()) {
+            return $this->currentShield;
+        }
+
+        if ($this->currentShield + $shieldAmount > $this->maxShield) {
+            $this->currentShield = $this->maxShield;
+            return $this->currentShield;
+        }
+
+        $this->currentShield += $shieldAmount;
+        return $this->currentShield;
     }
 
     public function heal(int $healAmount): int
     {
-        return $this->currentHealth += $healAmount;
+        if ($this->playerIsDead()) {
+            return $this->currentHealth;
+        }
+
+        if ($this->currentHealth + $healAmount > $this->maxHealth) {
+            $this->currentHealth = $this->maxHealth;
+            return $this->currentHealth;
+        }
+
+        $this->currentHealth += $healAmount;
+
+        return $this->currentHealth;
     }
 
     public function pickupItem(string $item): array
     {
-        return $this->inventory = $item;
-        $this->inventory;
+        if (count($this->inventory) >= $this->inventoryCapacity) {
+            return $this->inventory;
+        }
+
+        $this->inventory[] = $item;
+        return $this->inventory;
     }
 
-    public function damageAmount(int $damage): int
-    {}
+    public function dropItem(string $item): array
+    {
+        if (!in_array($item, $this->inventory)) {
+            return $this->inventory;
+        }
+
+        $indexToRemove = array_search($item, $this->inventory);
+        unset($this->inventory[$indexToRemove]);
+        return $this->inventory;
+    }
 
     private function dead(): void
     {
         echo "Better luck next time!";
     }
 
-    public function getStats(): string
+    private function playerIsDead(): bool
     {
-        return "HP: $this->currentHealth <br /> Max HP: $this->maxHealth <br /> Inventory: $this->inventory <br /> Inventory Capacity: $this->inventoryCapacity <br />";
+        return $this->currentHealth === 0;
     }
 }
 
-$player1 = new Player(200, 200, 3, []);
+$player = new Player(100, 100);
 
-echo $player1 -> getStats();
+$player->takeDamage(50);
+$player->takeDamage(50);
+$player->takeDamage(50);
+$player->takeDamage(50);
 
+echo '<pre>';
+var_dump($player);
